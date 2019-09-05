188974a6c2ea285e0e70d3bddd5fd92451eb6b22
lonmiss <1796887546@qq.com>
2019-09-05 09:12:22 +0800

    创建仓库

---
+++ java/workspace/class0905/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] argv) {
+	// write your code here
+        //System.out.println("hello JDK1.8~");
+
+    }
+}
---
+++ java/workspace/java/src/com/company/class20190903.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class class20190903 {
+    static public void main(String[] args)
+    {
+        System.out.println("hello world ");
+    }
+}
